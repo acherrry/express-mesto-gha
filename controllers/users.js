50dae41bf4c9d4698b4b5f01f7d793c41e198221
Alena Chernyadeva <alyonka_chernyadeva@mail.ru>
2022-08-24 13:05:22 +0300

    Добавить контроллеры и роуты для обновления аватара пользователя

--- controllers/users.js
+++ controllers/users.js
@@ -59,9 +59,28 @@ const editProfile = async (req, res) => {
   }
 };
 
+const editAvatar = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const { avatar } = req.body;
+    const user = await User
+      .findByIdAndUpdate(userId, { avatar }, { new: true });
+    if (!user) {
+      return res.status(NOT_FOUND_CODE).send({ message: 'Пользователь с указанным ID не найден' });
+    }
+    return res.send(user);
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      return res.status(ERROR_DATA_CODE).send({ message: 'Переданы некорректные данные при обновлении аватара' });
+    }
+    return res.status(ERROR_CODE).send({ message: 'Произошла ошибка на сервере' });
+  }
+};
+
 module.exports = {
   getUser,
   getUserById,
   createUser,
   editProfile,
+  editAvatar,
 };
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = require('express').Router();
 const {
-  getUser, getUserById, createUser, editProfile,
+  getUser, getUserById, createUser, editProfile, editAvatar,
 } = require('../controllers/users');
 
 router.get('/users', express.json(), getUser);
 router.get('/users/:userId', express.json(), getUserById);
 router.post('/users', express.json(), createUser);
 router.patch('/users/me', express.json(), editProfile);
+router.patch('/users/me/avatar', express.json(), editAvatar);
 
 module.exports = router;
